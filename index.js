b3fdb3c198caa099822645b499de76be4ca27afd
tomer <tomer.rajuan@gmail.com>
2020-04-23 14:05:21 +0200

    modal container working

--- index.js
+++ index.js
@@ -1,28 +1,72 @@
 (function() {
-  const x = document.getElementById("modal-container-close");
-  const learn_more_button = document.getElementById("learn-more-button");
+  const x1 = document.getElementById("modal-container-close1");
+  const x2 = document.getElementById("modal-container-close2");
+  const x3 = document.getElementById("modal-container-close3");
+  const x4 = document.getElementById("modal-container-close4");
+  const x5 = document.getElementById("modal-container-close5");
+  const x6 = document.getElementById("modal-container-close6");
+  const learn_more_button1 = document.getElementById("learn-more-button1");
+  const learn_more_button2 = document.getElementById("learn-more-button2");
+  const learn_more_button3 = document.getElementById("learn-more-button3");
+  const learn_more_button4 = document.getElementById("learn-more-button4");
+  const learn_more_button5 = document.getElementById("learn-more-button5");
+  const learn_more_button6 = document.getElementById("learn-more-button6");
+
   const welcome = document.getElementById("welcome");
 
-  x.addEventListener("click", function(event) {
-    console.log("we are here");
-    $(".modal-container").removeClass("on");
+  learn_more_button1.addEventListener("click", function() {
+      console.log("we are here");
+    $(".modal-container1").addClass("on");
+  });
+  learn_more_button2.addEventListener("click", function() {
+    $(".modal-container2").addClass("on");
+  });
+  learn_more_button3.addEventListener("click", function() {
+    $(".modal-container3").addClass("on");
+  });
+  learn_more_button4.addEventListener("click", function() {
+    $(".modal-container4").addClass("on");
+  });
+  learn_more_button5.addEventListener("click", function() {
+    $(".modal-container5").addClass("on");
+  });
+  learn_more_button6.addEventListener("click", function() {
+    $(".modal-container6").addClass("on");
   });
 
-  learn_more_button.addEventListener("click", function() {
-    $(".modal-container").addClass("on");
+  x1.addEventListener("click", function() {
+    console.log("we are here");
+    $(".modal-container1").removeClass("on");
+  });
+  x2.addEventListener("click", function() {
+    console.log("we are here");
+    $(".modal-container2").removeClass("on");
+  });
+  x3.addEventListener("click", function() {
+    console.log("we are here");
+    $(".modal-container3").removeClass("on");
+  });
+  x4.addEventListener("click", function() {
+    console.log("we are here");
+    $(".modal-container4").removeClass("on");
+  });
+  x5.addEventListener("click", function() {
+    console.log("we are here");
+
+    $(".modal-container5").removeClass("on");
+  });
+  x6.addEventListener("click", function() {
+    console.log("we are here");
+    $(".modal-container6").removeClass("on");
   });
 
   welcome.addEventListener("mouseover", function(event) {
-      var target =$(event.target);
-      var id = target.attr('id');
+    var target = $(event.target);
+    var id = target.attr("id");
 
     console.log("we are at logo event", id);
     animateDiv("#" + id);
-
   });
-
-
-
 
   function makeNewPosition() {
     // Get viewport dimensions (remove the dimension of the div)
@@ -37,9 +81,7 @@
 
   function animateDiv(myclass) {
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000, function() {
-
-    });
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000, function() {});
   }
   // $(window).scroll(function() {
   //   console.log(
@@ -66,23 +108,20 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-
-
-  $(window).scroll(function () {
-     var position = window.pageYOffset;
-     $('section').each(function () {
-       var target = $(this).offset().top;
-       console.log("position is",target);
-       var id = $(this).attr('id');
-       var navLinks = $('#navbar li a');
-       if (position >= target-300) {
-         navLinks.removeClass('active');
-         $('#navbar li a[href="#' + id + '"]').addClass('active');
-     } if(!position){
-  navLinks.removeClass("active");
-}
-     });
+  $(window).scroll(function() {
+    var position = window.pageYOffset;
+    $("section").each(function() {
+      var target = $(this).offset().top;
+      console.log("position is", target);
+      var id = $(this).attr("id");
+      var navLinks = $("#navbar li a");
+      if (position >= target - 300) {
+        navLinks.removeClass("active");
+        $('#navbar li a[href="#' + id + '"]').addClass("active");
+      }
+      if (!position) {
+        navLinks.removeClass("active");
+      }
+    });
   });
-
-
 })();
