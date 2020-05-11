fed7374572ed019c3540057b3574fd3edb48effe
tomer <tomer.rajuan@gmail.com>
2020-05-11 13:04:13 +0200

    css changes and gsap

--- index.js
+++ index.js
@@ -12,6 +12,26 @@
   const learn_more_button5 = document.getElementById("learn-more-button5");
   const welcome = document.getElementById("welcome");
 
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+
+const tl = new TimelineMax();
+
+tl.fromTo(
+  hero,
+  1,
+  {height: "0%"},
+  {height: "70%", ease: Power2.easeInOut}
+).fromTo(
+  hero,
+  1.2,
+  {width: "100%"},
+  {width: "100%", ease: Power2.easeInOut}
+)
+.fromTo(slider,1.2,{x: "-100%"}, {x: "0", ease: Power2.easeInOut},
+"-=1.2");
+
+
   learn_more_button1.addEventListener("click", function() {
     console.log("we are here");
     $(".modal-container1").addClass("on");
